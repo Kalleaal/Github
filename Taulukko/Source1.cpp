bc51612bb164fbb5c46dd73d97a379f74d688bce
Kalle Aaltonen <kalleaaltonen@kamk.fi>
2022-09-21 15:48:19 +0300

    uktui

---
+++ Taulukko/Source1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	int luvut[6]{ 0 };
+
+	for (int i = 0; i < 6; i++) 
+	{
+		// Kysytään lukua
+		cout << "Anna numero ";
+		cin >> luvut[i];
+	}
+
+	//tulostetaan taulukon sisältö
+	for (int = i; i < 6; i+2) //korjaa!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
